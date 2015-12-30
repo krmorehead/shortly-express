3170b7f55846f76e12b891751972424e0d55f842
krmorehead <krmorehead@gmail.com>
2015-12-29 19:32:46 -0800

    switched out out sessions for passport

--- shortly.js
+++ shortly.js
@@ -4,6 +4,8 @@ var partials = require('express-partials');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcrypt-nodejs');
 var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -23,13 +25,63 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(session({secret: 'danandkyle'}))
+app.use(passport.initialize());
+app.use(passport.session());
+/*
+app.configure(function() {
+  app.use(express.static('public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(app.router);
+});
+*/
 var sess;
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  new User({username : user}).fetch().then(function(user){
+    if(user){
+      done(null, user);
+    }else{
+      done(null, false, {message:'user does not exist'})
+    }
+  });
+});
+
+passport.use("login", new LocalStrategy(
+  {
+    passReqToCallback: true
+  },
+
+  function(req, username, password, done){
+  var username = req.body.username;
+  var password = req.body.password;
+  new User({username: username}).fetch().then(function(user){
+    if(user){
+      var hashPass = user.get('password');
+      bcrypt.compare(req.body.password, hashPass, function(err, response){
+        if(response){
+          done(null, user.attributes.username);
+        }else{
+          done(err);
+        }
+      });
+    }else{
+      done(null, false, {message: 'user does not exist'});
+    }
+  });
+}));
 
 app.get('/', 
 function(req, res) {
-  sess = req.session
-  if(sess.username){
-    res.render('index');
+  // sess = req.session
+  // if(sess.username){
+  if(req.isAuthenticated()){
+    res.render('index');    
   }else{
     res.redirect("/login")
   }
@@ -43,24 +95,21 @@ function(req, res){
 app.get('/logout',
 function(req, res){
   req.session.destroy();
-  console.log('its here')
   res.redirect('/login');
 })
 
 app.get('/create',
 function(req, res){ 
-  sess = req.session
-  if(sess.username){
-      res.render('index');
-    }else{
-      res.redirect("/login")
-    }
+  if(req.isAuthenticated()){
+    res.render('index');    
+  }else{
+    res.redirect("/login")
+  }
 });
 
 app.get('/links', 
 function(req, res) {
-  sess = req.session
-  if(sess.username){
+  if(req.isAuthenticated()){
     Links.reset().fetch().then(function(links) {
       res.send(200, links.models);
     });
@@ -76,36 +125,45 @@ function(req, res){
 
 app.post("/signup", 
 function(req, res){
-  // new User(req.body.username, req.body.password);
+  new User(req.body.username, req.body.password);
   util.passwordHash(req.body.password, function(err, pass){
     Users.create({
       username: req.body.username,
       password: pass
     }).then(function(something){
+      req.session.username = req.body.username;
       res.redirect('/');
     });
   });
 });
 
+
+// app.post('/login',
+//   passport.authenticate('local'),
+//   function(req, res) {
+//     // If this function gets called, authentication was successful.
+//     // `req.user` contains the authenticated user.
+//     res.redirect('/users/' + req.user.username);
+//   });
 app.post('/login', 
-function(req, res){
-  new User({username: req.body.username}).fetch().then(function(user){
-    if(user){
-      var hashPass = user.get('password');
-      bcrypt.compare(req.body.password, hashPass, function(err, response){
-        if(!err){
-          req.session.username = req.body.username
-          res.redirect("/");
-        }else{
-          res.redirect("/login");
-        }
-      });
-    }
-    else{
-      res.redirect("/login");
-    }
-  });
-});
+  passport.authenticate("login", {successRedirect : "/", failureRedirect : '/login'})
+  // new User({username: req.body.username}).fetch().then(function(user){
+  //   if(user){
+  //     var hashPass = user.get('password');
+  //     bcrypt.compare(req.body.password, hashPass, function(err, response){
+  //       if(!err){
+  //         req.session.username = req.body.username;
+  //         res.redirect("/");
+  //       }else{
+  //         res.redirect("/login");
+  //       }
+  //     });
+  //   }
+  //   else{
+  //     res.redirect("/login");
+  //   }
+  // });
+);
 
 // bcrypt.compare("bacon", hash, function(err, res) {
 //     // res == true
@@ -176,3 +234,15 @@ app.get('/*', function(req, res) {
 
 console.log('Shortly is listening on 4568');
 app.listen(4568);
+
+// app.post('/login', passport.authenticate('local', { successRedirect: '/',
+//                                                     failureRedirect: '/login' }));
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id);
+// });
+
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function(err, user) {
+//     done(err, user);
+//   });
+// });
